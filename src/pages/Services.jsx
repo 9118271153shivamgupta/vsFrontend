90769977d3797d6331b2d9fbb7f1b9c02d2189ec
Shivam Gupta <shivamkumargupta5592@gmail.com>
2026-02-28 23:11:29 +0530

    services page add kr diya gya

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
-import Footer from '../components/footer/Footer'
+import Footer from '../components/footer/Footer';
+import What_I_Do from '../components/whatIDo/What_I_Do';
 
 const Services = () => {
   return (
     <div>
+    <What_I_Do/>
       <Footer/>
       
       <h1> this is services page</h1>
